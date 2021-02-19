40321c37ac549d863a1656f69e40395442ffd381
Mohamed <benmoussa96med@gmail.com>
2021-02-19 14:52:01 +0100

    Update people-list.component.ts

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -10,33 +10,33 @@ coursList=[
   {
   id:1,
   Cours:"javascript",
-  title:"cours complet the javascript",
-  price:60
+  title:"cours  the javascript",
+  hours:15
 },
 {
   id:2,
-  Cours:"java",
-  title:"cours complet the java",
-  price:30
+  Cours:"Angular",
+  title:"cours the Angular",
+  hours:10
 },
 {
   id:3,
   Cours:"testcafe ",
-  title:"cours complet the testcafe (testing)",
-  price:29.99
+  title:"cours the testcafe (testing)",
+  hours:7
 },
 {
   id:4,
   Cours:"nestJs",
-  title:"cours complet the nestJs (backend)",
-  price:69.99
+  title:"cours the nestJs (backend)",
+  hours:6.5
 }
 ]
   constructor() { }
 
   ngOnInit(): void {
   }
-  delete(cours: { id: number; Cours: string; title: string; price: number; }){
+  delete(cours: { id: number; Cours: string; title: string; hours: number; }){
 let index=this.coursList.indexOf(cours);
 this.coursList.splice(index,1);
   }
