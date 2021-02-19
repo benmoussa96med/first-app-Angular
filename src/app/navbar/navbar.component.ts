587708930da6cbc95a43ef184cbb9116298748f3
Mohamed <benmoussa96med@gmail.com>
2021-02-19 13:59:45 +0100

    continue

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+imageUrl="assets/image/logo.png";
   constructor() { }
 
   ngOnInit(): void {
--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people-list.component.css']
 })
 export class PeopleListComponent implements OnInit {
-
+coursList=[
+  {
+  id:1,
+  Cours:"javascript",
+  title:"cours complet the javascript",
+  price:60
+},
+{
+  id:2,
+  Cours:"java",
+  title:"cours complet the java",
+  price:30
+},
+{
+  id:3,
+  Cours:"testcafe ",
+  title:"cours complet the testcafe (testing)",
+  price:29.99
+},
+{
+  id:4,
+  Cours:"nestJs",
+  title:"cours complet the nestJs (backend)",
+  price:69.99
+}
+]
   constructor() { }
 
   ngOnInit(): void {
+  }
+  delete(cours: { id: number; Cours: string; title: string; price: number; }){
+let index=this.coursList.indexOf(cours);
+this.coursList.splice(index,1);
   }
 
 }
